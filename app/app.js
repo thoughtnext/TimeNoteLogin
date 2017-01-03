bd82234bed372f76cf1a5d754dc5126e69afe451
Kritika <kritika@thoughtnext.com>
2017-01-03 20:12:37 +0530

    First commit

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 (function() {
-    var app = angular.module('app', ['ngRoute', 'ngMaterial','ngStorage'])
+    var app = angular.module('app', ['ngRoute', 'ngMaterial','ngStorage','angular-md5'])
 })();
